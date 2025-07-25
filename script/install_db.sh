eb5a9b330ed00bf644f3078e386b1d1923804e8a
luminousminer <luminousminer@gmail.com>
2025-07-25 16:33:23 +0200

    update install script

--- script/install_db.sh
+++ script/install_db.sh
@@ -1,6 +1,11 @@
-USERNAME=postgres
+USERNAME=luminousminer
 DATABASE=smart_mining
-FILE_INIT_DB=utils/db.sql
+FILE_INIT_DB=db.sql
+
+if ! psql -U $USERNAME -lqt | cut -d \| -f 1 | grep -qw $DATABASE; then
+    createdb -U $USERNAME $DATABASE
+fi
+
 
 psql\
  -U $USERNAME\
